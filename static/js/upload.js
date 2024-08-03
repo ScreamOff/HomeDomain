3b2f156394ccd985ab56a3472abd8af183c70ba8
ScreamOff <ancientone94@protonmail.com>
2024-08-03 02:49:22 +0200

    base version 2

--- static/js/upload.js
+++ static/js/upload.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const dropArea = document.getElementById('drop-area');
     const fileInput = document.getElementById('file-input');
     const fileNameDisplay = document.getElementById('file-name');
+    const uploadLabel = document.getElementById('upload-label');
 
     dropArea.addEventListener('dragover', (event) => {
         event.preventDefault();
@@ -16,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
         dropArea.classList.remove('dragover');
         const files = event.dataTransfer.files;
+        fileInput.files = files; // Update the file input with dropped files
         handleFiles(files);
     });
 
@@ -28,9 +30,8 @@ document.addEventListener('DOMContentLoaded', function () {
         fileNameDisplay.textContent = Array.from(files).map(file => file.name).join(', ') || 'Nie wybrano plików';
     }
 
-    // Dodanie obsługi kliknięcia na etykietę, aby otworzyć okno wyboru plików
-    const uploadButton = document.querySelector('label[for="file-input"]');
-    uploadButton.addEventListener('click', () => {
+    // Open file dialog when clicking on the label
+    uploadLabel.addEventListener('click', () => {
         fileInput.click();
     });
 });
